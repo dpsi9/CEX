36defa724d3dc257a7a533d902adad50d1ea140b
Dipendra Singh <dipendras953@gmail.com>
2025-11-15 01:42:15 +0530

    add global constants

---
+++ shared/src/constants.rs
@@ -0,0 +1,7 @@
+pub const DEFAULT_MARKET: &str = "SOLUSDC";
+pub const DEFAULT_DECIMAL_SCALE: u32 = 8;
+
+pub const EVENT_NEW_ORDER: &str = "order.new";
+pub const EVENT_CANCEL_ORDER: &str = "order.cancel";
+pub const EVENT_TRADE_EXECUTED: &str = "trade.executed";
+pub const EVENT_DB_SYNC: &str = "db.sync";
---
+++ shared/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod constants;
+pub mod events;
+pub mod utils;
+pub mod error;
+pub mod types;
+
